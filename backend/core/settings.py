c3d9f3770a85eccfbc79122fc0f5f0d893a706fa
Munetsi <zunguziramunetsi@gmail.com>
2025-06-29 08:25:21 +0100

    modified settings with new origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -175,10 +175,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "https://swopvend.vercel.app",
     "https://swopvend.com",
+    "https://frontend.swopvend.com"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://swopvend.vercel.app",
     "https://swopvend.com",
+    "https://frontend.swopvend.com"
 ]
 # If you use environment variables, e.g.:
 # CORS_ALLOWED_ORIGINS = [ os.environ['NEXT_PUBLIC_FRONTEND_URL'] ]
