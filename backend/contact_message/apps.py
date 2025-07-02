b64b8f10e2a29a5ee0b76be2d4da9a6c7c84ca60
Munetsi <zunguziramunetsi@gmail.com>
2025-07-02 23:20:43 +0100

    add signal discovery to contact

--- backend/contact_message/apps.py
+++ backend/contact_message/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ContactMessageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact_message'
+    
+    def ready(self):
+        # Ensure signals are registered
+        import contact_message.signals  # noqa
