b0616dc3168431c4123ee21a74048b278ec80bbb
Munetsi <zunguziramunetsi@gmail.com>
2025-07-26 19:28:18 +0100

    update settings for notification

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'django.contrib.sites',
-    'notifications',
+    'notification',
     'item',
     'trader',
     'swap',
